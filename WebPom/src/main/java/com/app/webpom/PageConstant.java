70182eea32969c47a0866bd5cf162a7d65500962
ludadha <ludadha@bestinet.com.my>
2014-12-29 15:59:37 +0800

    Add new file to GIT

---
+++ WebPom/src/main/java/com/app/webpom/PageConstant.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.app.webpom;
+
+/**
+ *
+ * @author bestinet
+ */
+public class PageConstant {
+    
+    
+    public final String HOME_PAGE = "home";
+    public final String IDM_PAGE = "idm";
+    
+}
